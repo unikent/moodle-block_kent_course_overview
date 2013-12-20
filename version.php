f86c301d0ca595ce788efcbcf3420774f32f1f2d
Jake Blatchford <J.Blatchford@kent.ac.uk>
2013-12-20 15:26:24 +0000

    Upped version number of kent course overview block

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013120600;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2013122000;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2011112900;        // Requires this Moodle version
 $plugin->component = 'block_kent_course_overview'; // Full name of the plugin (used for diagnostics)
