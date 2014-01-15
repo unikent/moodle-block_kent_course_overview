a4613ab09c06db1dc88c32c1fcbdf117792595eb
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-01-15 11:10:32 +0000

    Bump KCO version

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014010306;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2014011500;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2011112900;        // Requires this Moodle version
 $plugin->component = 'block_kent_course_overview'; // Full name of the plugin (used for diagnostics)
