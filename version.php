bd0705394e5061dc257dd39018f7751b414722a8
Jake Blatchford <J.Blatchford@kent.ac.uk>
2015-05-14 13:40:42 +0100

    Add observer for cla rollover update

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015051400;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2015051401;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013110500;        // Requires this Moodle version
 $plugin->component = 'block_kent_course_overview'; // Full name of the plugin (used for diagnostics).
 
