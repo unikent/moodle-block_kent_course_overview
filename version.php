73f06d4126107ebe49e56bf43044c1bd58beb3d4
Skylar Kelty <s.kelty@kent.ac.uk>
2015-06-11 13:47:45 +0100

    We use simple keys

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015060500;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2015061100;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013110500;        // Requires this Moodle version
 $plugin->component = 'block_kent_course_overview'; // Full name of the plugin (used for diagnostics).
 
